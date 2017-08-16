0ad99867b68163d287ca52b077e7ec300b3ba353
Guy Tristram <guy.tristram@digital.hmrc.gov.uk>
2017-08-16 17:38:08 +0100

    some data saved

--- app/routes.js
+++ app/routes.js
@@ -13,5 +13,53 @@ router.get('/signin-output', function (req, res) {
 })
 
 
+// CREATE EVENT ONWARDS TO NEXT PAGE
+router.get('/create-event/create-event-onwards', function (req, res) {
+
+  // Save event date
+  req.session.data['event-day'];
+  req.session.data['event-month'];
+  req.session.data['event-year'];
+
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  if(req.session.data['event-month'] <= 13)
+  { req.session.data['event-month'] =  monthNames[req.session.data['event-month']-1] }
+
+
+  //SAVE THE TIMES
+  var finishHour = req.session.data['finish-hours'].toString();
+  req.session.data['event-start-time'] = req.session.data['start-hours'] + ":" + req.session.data['start-minutes'];
+  req.session.data['event-finish-time'] =  req.session.data['finish-hours'] + ":" + req.session.data['finish-minutes'];
+
+  res.redirect('/create-event/venue');
+})
+
+
+// VENUE PAGE ONWARDS BUTTON
+router.get('/create-event/venue-onwards', function (req, res) {
+
+  req.session.data['event-title'];
+  req.session.data['event-description'];
+
+  res.redirect('/create-event/description');
+
+})
+
+
+// DESCRIPTION PAGE ONWARDS BUTTON
+router.get('/create-event/description-onwards', function (req, res) {
+
+  req.session.data['event-title'];
+  req.session.data['event-description'];
+
+  res.redirect('/create-event/attendees');
+
+})
+
+
+
+
 
 module.exports = router
